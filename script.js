10fff4a7d892a5dc7a17a7e3ce5ca8dfaa3858cd
Manoj Modhwadia <manojmodhwadia@outlook.com>
2019-02-06 15:26:25 +0000

    added some styling to nav

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ $(".jumper").on("click", function(e) {
   );
 });
 
-$(document).ready(function() {
-  $("sidenav").sidenav();
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, options);
 });
+
+// $(document).ready(function() {
+//   $(".sidenav").sidenav();
+// });
