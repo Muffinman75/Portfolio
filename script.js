8a83f87f806cb28656967a6b1829a0d892471b7b
Manoj Modhwadia <manojmodhwadia@outlook.com>
2019-02-07 14:12:34 +0000

    positioned elements in footer

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(".jumper").on("click", function(e) {
-  e.preventDefault();
-
-  $("body, html").animate(
-    {
-      scrollTop: $($(this).attr("href")).offset().top
-    },
-    600
-  );
-});
+// $(".jumper").on("click", function(e) {
+//   e.preventDefault();
+//
+//   $("body, html").animate(
+//     {
+//       scrollTop: $($(this).attr("href")).offset().top
+//     },
+//     600
+//   );
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".sidenav");
